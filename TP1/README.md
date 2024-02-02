7d65e2c36d8ac1579933299556d0f211264104e9
tarik <77856978+ItsTarikBTW@users.noreply.github.com>
2024-02-03 00:19:50 +0100

    TP1 exo morse code

--- TP1/README.md
+++ TP1/README.md
@@ -9,8 +9,8 @@
 ### example.ino
 this code is a simple example of how to use the arduino board to control a LED and Piezo .
 
-### morceCode.ino
-PLACEHOLDER
+### morseCode.ino
+this code is the implementation of the morse code using the arduino board and a Piezo.
 
 ## Footer
 this is a summary of the first TP of the course ARDUINO for Master 1 software engineering at the University of Tlemcen 2023/2024 season. Written by [Tarik HAMMOUMI](https://github.com/ItsTarikBTW).
---
+++ TP1/morseCode.ino
@@ -0,0 +1,67 @@
+#define TimeDot 100 
+#define TimeDash TimeDot*3
+#define TimeSpaceBC 500 //Between characters
+#define TimeSpaceBW 700 //Between words
+int led;
+int piezo;
+char* input="tarik sos";
+int i=0;
+
+const char* morse[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."};
+
+const char chars[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+void trigger(int duration){
+    digitalWrite(led, HIGH); 
+    tone(piezo, 600,duration); 
+    delay(duration); 
+    digitalWrite(led, LOW);
+}
+void setup()
+{
+  led=9;
+  piezo=6;
+  pinMode(led, OUTPUT);
+  pinMode(piezo, OUTPUT);
+  Serial.begin(9600);
+}
+
+void toMorseCode(char c) {
+  if (c == ' ') {
+    delay(TimeSpaceBW);
+  } else {
+    int index = findCharIndex(c);
+    if (index != -1) {
+      Serial.println(morse[index]);
+      playMorseCode(morse[index]);
+    }
+  }
+}
+
+int findCharIndex(char c) {
+  for (int j = 0; j < 36; j++) {
+    if (c == chars[j]) {
+      return j;
+    }
+  }
+  return -1;
+}
+
+void playMorseCode(const char* morseCode) {
+  for (int k = 0; k < strlen(morseCode); k++) {
+    if (morseCode[k] == '-') {
+      trigger(TimeDash);
+    } else if (morseCode[k] == '.') {
+      trigger(TimeDot);
+    }
+    delay(TimeSpaceBC);
+  }
+}
+
+void loop()
+{
+  if(i<strlen(input)){
+    toMorseCode(toupper(input[i]));
+    i++;
+  }
+}
